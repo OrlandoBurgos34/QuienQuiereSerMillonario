b659be4f3cd481d6e7448bcf8942fe0c769d8995
orlando <programador3423@gmail.com>
2023-11-08 11:35:15 -0500

    - configuración de avance hacia la tercera vista

--- QuienQuiereSerMillonario/InterfaceGameViewController.swift
+++ QuienQuiereSerMillonario/InterfaceGameViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class InterfaceDeJuegoViewController: UIViewController {
+class InterfaceGameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
