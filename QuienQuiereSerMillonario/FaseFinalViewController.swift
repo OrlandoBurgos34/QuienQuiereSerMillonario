c1fec725ad253ee617503da297c8358eac13f1a7
orlando <programador3423@gmail.com>
2023-11-14 09:04:49 -0500

    -implementacion de variables, funciones y label de la ultima vista

--- QuienQuiereSerMillonario/FaseFinalViewController.swift
+++ QuienQuiereSerMillonario/FaseFinalViewController.swift
@@ -14,19 +14,40 @@ class FaseFinalViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+    var preguntasCorrectas: [Pregunta] = []
+    var preguntasIncorrectas: [Pregunta] = []
+    var intentosFallidos: Int = 0
+    var puntosGanados: Int = 0
+    var userName: String = ""
     @IBOutlet weak var viewPointTextLabel: UILabel!
-    
+    @IBOutlet weak var acertadasLabel: UILabel!
+    @IBOutlet weak var fallidasLabel: UILabel!
+    @IBOutlet weak var preguntasCorrectasLabel: UILabel!
+    @IBOutlet weak var preguntasIncorrectasLabel: UILabel!
     @IBAction func resetGameButton(_ sender: UIButton) {
+        reiniciarJuego()
+        navigationController?.popToRootViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func reiniciarJuego() {
+        preguntasCorrectas = []
+        preguntasIncorrectas = []
+        intentosFallidos = 0
+        puntosGanados = 0
     }
-    */
-
+    func calcularPuntajeFinal() -> Int {
+        var finalPoints = preguntasIncorrectas.count * 10
+        if preguntasCorrectas.count == 10{
+            return puntosGanados - finalPoints
+        } else {
+            finalPoints = preguntasCorrectas.count * 10
+            return finalPoints
+        }
+    }
+    func obtenerTextoPreguntas(_ preguntas: [Pregunta]) -> String {
+        var textoPreguntas = ""
+        for pregunta in preguntas {
+            textoPreguntas += "\(pregunta.enunciado)\n"
+        }
+        return textoPreguntas
+    }
 }
